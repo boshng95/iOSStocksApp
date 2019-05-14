39be0d4733a439b4ab75f23c14412735fa4a3de5
boshng95 <ngjun95@gmail.com>
2019-05-14 14:07:46 +1000

    DTask/

---
+++ DTask/DTask/Company.swift
@@ -0,0 +1,9 @@
+//
+//  Company.swift
+//  DTask
+//
+//  Created by Tammy Sim on 06/05/2019.
+//  Copyright © 2019 Bosh Ng. All rights reserved.
+//
+
+import Foundation
---
+++ DTask/DTask/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  DTask
+//
+//  Created by Tammy Sim on 08/05/2019.
+//  Copyright © 2019 Bosh Ng. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
